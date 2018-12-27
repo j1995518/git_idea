a2c52c81cc0e8efef180f08b93be64d2026cc179
user <1525687730@qq.com>
2018-12-27 20:01:34 +0800

    初始化工程.......

---
+++ test/demo/src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Desc
+ * @Author 123
+ * @CreateTime 2018/12/27 19:53
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("135653523453");
+    }
+}
+
